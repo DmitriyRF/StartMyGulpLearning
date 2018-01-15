e2caa8875b2bd41952383b5f828f7befcff4eccc
Dima Romanovskiy <Dmitriy_r_f@mail.ru>
2018-01-15 15:31:11 +0300

    rename directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
-var 	gulp		=		require('gulp'),
-		less		=		require('gulp-less');
+var 	gulp			=		require('gulp'),
+		less			=		require('gulp-less'),
+		browserSync		=		require('browser-sync').create();
 
 
 gulp.task(  'less-compilation', function () {
 
 	return gulp.src('./assets//less/**/*.less')
 				.pipe(  less()  )
-				.pipe(  gulp.dest('./assets/css')  );
+				.pipe(  gulp.dest('./assets/css')  )
+				.pipe(  browserSync.stream()  );
 });
 
-gulp.task(  'watch', function(){
+gulp.task(  'watch', ['browser-sync', 'less-compilation'], function(){
 
 	gulp.watch(  './assets/less/**/*.less', ['less-compilation']  );
 
+	gulp.watch(  './assets/*.html',  browserSync.reload  );
+
+	gulp.watch(  './assets/**/*.js',  browserSync.reload  );
+
+	// browserSync.watch(  "./assets/*.html").on("change", browserSync.reload  );
+
+});
+
+
+gulp.task(  'browser-sync', function(){
+	browserSync.init({
+		server: 'assets'
+	});
 });
